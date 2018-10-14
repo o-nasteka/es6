f4c898b125a055d7626011aef301d5d338b6e4c5
nasteka <oleg.nasteka@gmail.com>
2018-10-14 21:11:26 +0300

    object

--- index.js
+++ index.js
@@ -14,8 +14,47 @@
 // console.log(arrow3(10));
 
 
-const func = (a = 20) => {
-    return a;
+// const func = (a = 20) => {
+//     return a;
+// };
+//
+// console.log(func());
+
+// Func END
+
+// Object Start
+
+const name = 'WFM';
+const age = 30;
+
+// ES5
+// const obj = {
+//     name: name,
+//     age: age
+// };
+
+// console.log(obj);
+
+// ES6
+// const obj = {name,age};
+// console.log(obj);
+
+// Object END
+
+const createPerson = (name, surname, fieldName, fieldPostfix) => {
+    const fullname = name + ' ' + surname;
+    return {
+        fullname,
+        name,
+        surname,
+        getJob() {
+            return 'Front End';
+        },
+        [fieldName + fieldPostfix]: 100
+    };
 };
 
-console.log(func());
+const person = createPerson('Vasya', 'Petrov', 'car', '-name');
+
+// console.log(person.getJob());
+console.log(person);
